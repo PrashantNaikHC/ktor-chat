fd65959c85959558755ebae24d8da7f55384a88f
prashant naik <real.hyper.clocked@gmail.com>
2021-10-26 00:17:07 +0530

    Adding chat client

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,4 +16,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("io.ktor:ktor-websockets:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("io.ktor:ktor-client-websockets:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
 }
---
+++ src/main/kotlin/com/hyperclock/ChatClient.kt
@@ -0,0 +1,27 @@
+package com.hyperclock
+
+import io.ktor.client.*
+import io.ktor.client.features.websocket.*
+import io.ktor.http.*
+import io.ktor.http.cio.websocket.*
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    val client = HttpClient {
+        install(WebSockets)
+    }
+    runBlocking {
+        client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
+            while(true) {
+                val othersMessage = incoming.receive() as? Frame.Text ?: continue
+                println(othersMessage.readText())
+                val myMessage = readLine()
+                if(myMessage != null) {
+                    send(myMessage)
+                }
+            }
+        }
+    }
+    client.close()
+    println("Connection closed. Goodbye!")
+}
